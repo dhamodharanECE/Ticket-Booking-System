f135cb46c3e6c8b1902964bdf57f3752ad8fabb2
DHAMODHARAN S <dhamodharansece23@gmail.com>
2025-12-12 19:26:20 +0530

    frontend

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = "mongodb+srv://dhamodharans:pass123@cluster0.sdep6.mongodb.net/ticket_booking"; 
+    const mongoURI = 'mongodb+srv://dhamodharans:pass123@cluster0.sdep6.mongodb.net/ticket_booking'; 
     console.log("Attempting to connect to:", mongoURI);
 
     const conn = await mongoose.connect(mongoURI);
--- frontend/project/src/services/api.js
+++ frontend/project/src/services/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://ticket-booking-system-1-s01t.onrender.com/api' });
 
 export const fetchShows = () => API.get('/shows');
 export const fetchShowById = (id) => API.get(`/shows/${id}`);
 export const createShow = (showData) => API.post('/admin/shows', showData);
 export const bookSeats = (bookingData) => API.post('/shows/book', bookingData);
-
-
 
 export default API;
